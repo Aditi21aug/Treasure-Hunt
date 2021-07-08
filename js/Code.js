0de5845d80a69b5ad1d44c2f0a22ddfdacd66eb3
Aditi <82038181+Aditi21aug@users.noreply.github.com>
2021-07-08 06:27:14 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,33 @@
+class Code
+{
+    constructor()
+    {
+        this.accessCode1 = "VARIABLE";
+        this.accessCode2 = "FUNCTION";
+        this.accessCode3 = "DATABASE";
+        
+    }
+
+clues() 
+{
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("O U F C N T I ", 700,150)
+    fill("lightblue")
+    text("Hint: Called to complete a specific task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T D S E A B", 500,250)
+    fill("lightblue")
+    text("Hint: Used to store data !!", 500,270)
+    
+}
+}
